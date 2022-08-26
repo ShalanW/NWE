edbcec5ee1df69ca51c5d3997f730f56d26b47ce
Shalan <shalan@thewiseapps.com>
2022-08-26 14:42:30 -0400

    firebase

--- NWE/src/environments/environment.prod.ts
+++ NWE/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyD2UmdLvwZaoyakTR9KqEt45UsKvWGvxW0",
+    authDomain: "nwenv-a1b35.firebaseapp.com",
+    projectId: "nwenv-a1b35",
+    storageBucket: "nwenv-a1b35.appspot.com",
+    messagingSenderId: "572375867631",
+    appId: "1:572375867631:web:eca10209a7bed29d8a3b1a",
+    measurementId: "G-2MTV6MTKQ7"
+  }
 };
--- NWE/src/environments/environment.ts
+++ NWE/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyD2UmdLvwZaoyakTR9KqEt45UsKvWGvxW0",
+    authDomain: "nwenv-a1b35.firebaseapp.com",
+    projectId: "nwenv-a1b35",
+    storageBucket: "nwenv-a1b35.appspot.com",
+    messagingSenderId: "572375867631",
+    appId: "1:572375867631:web:eca10209a7bed29d8a3b1a",
+    measurementId: "G-2MTV6MTKQ7"
+  }
 };
 
 /*
