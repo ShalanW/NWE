c0f6a06591cb874a5fb8fcdd37ec9e05e7cbc628
Shalan <shalan@thewiseapps.com>
2022-09-15 10:44:14 -0400

    Create todo.ts

---
+++ NWE/src/app/notes/todo.ts
@@ -0,0 +1,17 @@
+/*
+TODO: Bill Fixer - add:
+   - WM calculates RCRon fuel and env
+   - Use New Sunset Bay as example
+   - According to WM:
+   - ENV = 20% and RCR = 3.6%
+*/
+
+/*
+TODO: On-Call Accounts - add:
+   - Account Notes
+   - Customer Use New Sunset Bay as example
+*/
+
+/*
+TODO: FIX PICKWICK!!
+*/
